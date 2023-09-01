aa0bdc0281d0b07ebdfc59ad1d97493146c8dd14
Muhammad Asim <73778163+devistic-asim@users.noreply.github.com>
2023-09-02 02:06:32 +0500

    Update Program.cs

--- coderush/Program.cs
+++ coderush/Program.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace coderush
+namespace coderush 
 {
     public class Program
     {
